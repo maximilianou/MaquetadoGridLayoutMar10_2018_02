394ead8f902c32b9ad35156ab830dd48e7849b07
Glitch (maquetadogridlayoutmar10-2018-02) <none>
2018-07-10 14:23:46 +0000

    🎀🌓 Updated with Glitch

---
+++ cliente.js
@@ -0,0 +1,19 @@
+// Programando una clase en Javascript
+class Notas{
+  // Programando un metodo de una clase en Javascript
+  static subir(){
+  
+    console.log('[..] Hola, martes con frio en buenos aires!!!');
+
+    alert('Huy, Aca se detiene el Programa y puedo Verificar Variables y Valores');
+
+    console.log('[OK] Hola, martes con frio en buenos aires!!!');
+    
+  }
+  
+}
+
+
+
+
+
