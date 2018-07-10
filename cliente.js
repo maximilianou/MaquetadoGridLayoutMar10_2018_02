ef2a864df033b6ad76528bf255720d58269166b6
Glitch (maquetadogridlayoutmar10-2018-02) <none>
2018-07-10 15:39:26 +0000

    🏂👜 Updated with Glitch

--- cliente.js
+++ cliente.js
@@ -2,13 +2,32 @@
 class Notas{
   // Programando un metodo de una clase en Javascript
   static subir(){
-  
+
     console.log('[..] Hola, martes con frio en buenos aires!!!');
 
-    alert('Huy, Aca se detiene el Programa y puedo Verificar Variables y Valores');
+    document.querySelector("#panelMsgContenido").innerHTML
+        += document.querySelector( "#nota_titulo" ).value;
 
-    console.log('[OK] Hola, martes con frio en buenos aires!!!');
+    document.querySelector( "#nota_titulo" ).value = "";
     
+/*    
+    document.querySelector("#panelMsg").innerHTML
+        += "Mensaje desde el Codigo, DATO!!";
+*/
+/*
+    document.querySelector("section > h1").innerHTML
+        += "Mensaje desde el Codigo, DATO!!";
+*/
+    
+    
+//    alert('Huy, Aca se detiene el Programa y puedo Verificar Variables y Valores');
+    
+    console.log('[OK] Hola, martes con frio en buenos aires!!!');
+
+  }
+
+  static limpiar(){
+    document.querySelector("#panelMsgContenido").innerHTML = "";
   }
   
 }
